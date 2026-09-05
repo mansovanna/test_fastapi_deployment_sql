1d2d0d31e26c2f9bcc288134aa8050a8d55b5b1d
mansovanna <mansovanna2022@gmail.com>
2026-09-05 16:10:56 +0700

    Update Database use Pogress

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
-#database.py
+# database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://test_db_vbbs_user:nxCVgqlGbSwtblM0TlePi4SzN2xGSOdG@dpg-dadpp8id0e5s73dsq1mg-a.oregon-postgres.render.com/test_db_vbbs?sslmode=require"
+)
 
-
-
-engine = create_engine(DATABASE_URL, connect_args= {"check_same_thread": False})
+engine = create_engine(DATABASE_URL)  # no connect_args here
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
